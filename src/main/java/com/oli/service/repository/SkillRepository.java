095986db6b27846be5c9cddeee8756d249d077d9
Sambuddha Chaudhuri <sambuddhachaudhuri20@gmail.com>
2017-08-08 22:15:01 +0530

    Adding Repository Classes to check the Demo

---
+++ src/main/java/com/oli/service/repository/SkillRepository.java
@@ -0,0 +1,9 @@
+package com.oli.service.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.oli.service.entity.Skill;
+
+public interface SkillRepository extends CrudRepository<Skill, Integer> {
+
+}
