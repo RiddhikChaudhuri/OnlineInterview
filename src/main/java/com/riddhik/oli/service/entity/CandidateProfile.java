26b2450abf83fcd9a68329400a94e05e1e4a72e8
Sambuddha Chaudhuri <sambuddhachaudhuri20@gmail.com>
2017-08-15 20:54:48 +0530

    Modifying the Entity Class

--- src/main/java/com/riddhik/oli/service/entity/CandidateProfile.java
+++ src/main/java/com/riddhik/oli/service/entity/CandidateProfile.java
@@ -11,8 +11,7 @@ import javax.persistence.OneToOne;
 
 @Embeddable
 public class CandidateProfile {
-    
-        
+
     @Column
     private String position;
 
@@ -36,77 +35,88 @@ public class CandidateProfile {
     @Embedded
     private EducationDetails educationDeatils;
 
-    public String getPosition() {
-        return position;
+    public CandidateProfile() {
+	super();
     }
 
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Integer getExperience() {
-        return experience;
-    }
-
-    public void setExperience(Integer experience) {
-        this.experience = experience;
-    }
-
-    public List<Skill> getSkills() {
-        return skills;
-    }
-
-    public void setSkills(List<Skill> skills) {
-        this.skills = skills;
-    }
-
-    public String getJobLocation() {
-        return jobLocation;
-    }
-
-    public void setJobLocation(String jobLocation) {
-        this.jobLocation = jobLocation;
+    public CandidateProfile(String position, Integer experience, List<Skill> skills, String jobLocation,
+	    String costToCompany, Integer timeToJoin, EducationDetails educationDeatils) {
+	super();
+	this.position = position;
+	this.experience = experience;
+	this.skills = skills;
+	this.jobLocation = jobLocation;
+	this.costToCompany = costToCompany;
+	this.timeToJoin = timeToJoin;
+	this.educationDeatils = educationDeatils;
     }
 
     public String getCostToCompany() {
-        return costToCompany;
-    }
-
-    public void setCostToCompany(String costToCompany) {
-        this.costToCompany = costToCompany;
-    }
-
-    public Integer getTimeToJoin() {
-        return timeToJoin;
-    }
-
-    public void setTimeToJoin(Integer timeToJoin) {
-        this.timeToJoin = timeToJoin;
+	return costToCompany;
     }
 
     @OneToOne(cascade = CascadeType.ALL)
     public EducationDetails getEducationDeatils() {
-        return educationDeatils;
+	return educationDeatils;
+    }
+
+    public Integer getExperience() {
+	return experience;
+    }
+
+    public String getJobLocation() {
+	return jobLocation;
+    }
+
+    public String getPosition() {
+	return position;
+    }
+
+    public List<Skill> getSkills() {
+	return skills;
+    }
+
+    public Integer getTimeToJoin() {
+	return timeToJoin;
+    }
+
+    public void setCostToCompany(String costToCompany) {
+	this.costToCompany = costToCompany;
     }
 
     public void setEducationDeatils(EducationDetails educationDeatils) {
-        this.educationDeatils = educationDeatils;
+	this.educationDeatils = educationDeatils;
     }
 
-    public CandidateProfile(String position, Integer experience, List<Skill> skills, String jobLocation,
-            String costToCompany, Integer timeToJoin, EducationDetails educationDeatils) {
-        super();
-        this.position = position;
-        this.experience = experience;
-        this.skills = skills;
-        this.jobLocation = jobLocation;
-        this.costToCompany = costToCompany;
-        this.timeToJoin = timeToJoin;
-        this.educationDeatils = educationDeatils;
+    public void setExperience(Integer experience) {
+	this.experience = experience;
     }
 
-    public CandidateProfile() {
-        super();
+    public void setJobLocation(String jobLocation) {
+	this.jobLocation = jobLocation;
+    }
+
+    public void setPosition(String position) {
+	this.position = position;
+    }
+
+    public void setSkills(List<Skill> skills) {
+	this.skills = skills;
+    }
+
+    public void setTimeToJoin(Integer timeToJoin) {
+	this.timeToJoin = timeToJoin;
+    }
+
+    @Override
+    public String toString() {
+	return "CandidateProfile [position=" + position + ", experience=" + experience + ", skills=" + skills
+		+ ", jobLocation=" + jobLocation + ", costToCompany=" + costToCompany + ", timeToJoin=" + timeToJoin
+		+ ", educationDeatils=" + educationDeatils + ", getPosition()=" + getPosition() + ", getExperience()="
+		+ getExperience() + ", getSkills()=" + getSkills() + ", getJobLocation()=" + getJobLocation()
+		+ ", getCostToCompany()=" + getCostToCompany() + ", getTimeToJoin()=" + getTimeToJoin()
+		+ ", getEducationDeatils()=" + getEducationDeatils() + ", getClass()=" + getClass() + ", hashCode()="
+		+ hashCode() + ", toString()=" + super.toString() + "]";
     }
 
 }
--- src/main/java/com/riddhik/oli/service/entity/EducationDetails.java
+++ src/main/java/com/riddhik/oli/service/entity/EducationDetails.java
@@ -15,39 +15,48 @@ public class EducationDetails {
     @Column(name = "DCREQUIREMENT")
     private Boolean doctrateRequirement;
 
-    public Boolean getUgRequirement() {
-        return ugRequirement;
-    }
-
-    public void setUgRequirement(Boolean ugRequirement) {
-        this.ugRequirement = ugRequirement;
-    }
-
-    public Boolean getPgRequirement() {
-        return pgRequirement;
-    }
-
-    public void setPgRequirement(Boolean pgRequirement) {
-        this.pgRequirement = pgRequirement;
-    }
-
-    public Boolean getDoctrateRequirement() {
-        return doctrateRequirement;
-    }
-
-    public void setDoctrateRequirement(Boolean doctrateRequirement) {
-        this.doctrateRequirement = doctrateRequirement;
-    }
-
     public EducationDetails() {
-        super();
+	super();
     }
 
     public EducationDetails(Boolean ugRequirement, Boolean pgRequirement, Boolean doctrateRequirement) {
-        super();
-        this.ugRequirement = ugRequirement;
-        this.pgRequirement = pgRequirement;
-        this.doctrateRequirement = doctrateRequirement;
+	super();
+	this.ugRequirement = ugRequirement;
+	this.pgRequirement = pgRequirement;
+	this.doctrateRequirement = doctrateRequirement;
+    }
+
+    public Boolean getDoctrateRequirement() {
+	return doctrateRequirement;
+    }
+
+    public Boolean getPgRequirement() {
+	return pgRequirement;
+    }
+
+    public Boolean getUgRequirement() {
+	return ugRequirement;
+    }
+
+    public void setDoctrateRequirement(Boolean doctrateRequirement) {
+	this.doctrateRequirement = doctrateRequirement;
+    }
+
+    public void setPgRequirement(Boolean pgRequirement) {
+	this.pgRequirement = pgRequirement;
+    }
+
+    public void setUgRequirement(Boolean ugRequirement) {
+	this.ugRequirement = ugRequirement;
+    }
+
+    @Override
+    public String toString() {
+	return "EducationDetails [ugRequirement=" + ugRequirement + ", pgRequirement=" + pgRequirement
+		+ ", doctrateRequirement=" + doctrateRequirement + ", getUgRequirement()=" + getUgRequirement()
+		+ ", getPgRequirement()=" + getPgRequirement() + ", getDoctrateRequirement()="
+		+ getDoctrateRequirement() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+		+ ", toString()=" + super.toString() + "]";
     }
 
 }
